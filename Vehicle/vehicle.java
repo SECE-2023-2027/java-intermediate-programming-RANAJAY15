e70b7f2a950b8df0cab8c95e48e2ee73993c9508
RANAJAY2005 <ranajay.s2021@gmail.com>
2026-07-01 14:02:40 +0530

    problem 5

---
+++ Vehicle/vehicle.java
@@ -0,0 +1,58 @@
+package Vehicle;
+import java.util.Scanner;
+
+class Vehicle {
+    public String startEngine() {
+        return "Vehicle engine started";
+    }
+}
+
+class Car extends Vehicle {
+    @Override
+    public String startEngine() {
+        return "Car engine roars";
+    }
+}
+
+class Motorcycle extends Vehicle {
+    @Override
+    public String startEngine() {
+        return "Motorcycle engine revs";
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Vehicle vehicle;
+
+        System.out.println("Select Vehicle Type");
+        System.out.println("1. Car");
+        System.out.println("2. Motorcycle");
+        System.out.print("Enter your choice: ");
+        int choice = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                vehicle = new Car();
+                break;
+
+            case 2:
+                vehicle = new Motorcycle();
+                break;
+
+            default:
+                vehicle = new Vehicle();
+                break;
+        }
+
+        System.out.println("\nEngine Status:");
+        System.out.println(vehicle.startEngine());
+
+        sc.close();
+    }
+}
+public class vehicle {
+    
+}
